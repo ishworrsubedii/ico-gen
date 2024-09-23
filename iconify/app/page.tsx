a98e8996ada5da48b7eaa818341e7b90ff6021d6
Ishwor Subedi <ishworr.subedi@gmail.com>
2024-09-23 17:58:56 +0545

    chore: add requirements.txt file

--- iconify/app/page.tsx
+++ iconify/app/page.tsx
@@ -1,5 +1,43 @@
-import { Layout } from "../components/layout"
 
-export default function Page() {
-  return <Layout />
+'use client'
+
+import {useState, useEffect} from 'react'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import HeroSection from '@/components/hero-section'
+import HowItWorksSection from '@/components/how-it-works'
+import UpcomingFeaturesSection from '@/components/upcoming-features'
+import CallToActionSection from '@/components/call-to-action'
+
+
+export default function HomepageLayout() {
+    const [isDarkMode, setIsDarkMode] = useState(false)
+
+    useEffect(() => {
+        document.body.classList.toggle('dark', isDarkMode)
+    }, [isDarkMode])
+
+    const toggleTheme = () => {
+        setIsDarkMode(!isDarkMode)
+    }
+
+    return (
+        <div className={`min-h-screen ${isDarkMode ? 'dark' : ''}`}>
+            <div className="bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+                <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme}/>
+                <main className="container mx-auto px-4 py-12">
+                    <HeroSection/>
+                    <HowItWorksSection/>
+                    <br></br>
+                    <UpcomingFeaturesSection/>
+                    <br></br>
+                    <CallToActionSection/>
+                    <br></br>
+                    
+                </main>
+                <Footer/>
+            </div>
+        </div>
+    )
 }
+
